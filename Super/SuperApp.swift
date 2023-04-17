4db506c6b97850b0ab0a99b55a593212d4c6cb5c
hebaabdullah1 <127639276+hebaabdullah1@users.noreply.github.com>
2023-04-17 07:48:27 +0200

    Initial Commit

---
+++ Super/SuperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperApp.swift
+//  Super
+//
+//  Created by Shimaa Elcc on 17.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
